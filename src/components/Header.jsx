9e5ccde1fba7baa1d116bf6aae9619db5286f749
Igor Pomazkov <igorpomazzzkov@gmail.com>
2020-11-07 10:42:26 +0000

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (<header className="header">
-      <img src="https://banner2.cleanpng.com/20190301/u/kisspng-logo-font-brand-clip-art-product-autograph-icon-1-24x1-24px-ico-png-icns-free-5c792877caedc8.5361910515514440878312.jpg"/>
+      <img src="https://cdn3.iconfinder.com/data/icons/social-media-2169/24/social_media_social_media_logo_imo-256.png"/>
     </header>)
 }
 
