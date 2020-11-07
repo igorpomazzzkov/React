72186dae7f8db7f610f7c60b6c1f7ee4c9eba336
Igor Pomazkov <igorpomazzzkov@gmail.com>
2020-11-07 10:34:50 +0000

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (<header className="header">
+      <img src="https://banner2.cleanpng.com/20190301/u/kisspng-logo-font-brand-clip-art-product-autograph-icon-1-24x1-24px-ico-png-icns-free-5c792877caedc8.5361910515514440878312.jpg"/>
+    </header>)
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+const Profile = () =>{
+    return (
+        <div className="content">
+      <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTbySopxhYHbpzaomZQdtSfnC_u0Fo3fXECNQ&usqp=CAU"></img>
+      </div>
+    </div>
+    )
+}
+
+export default Profile;
